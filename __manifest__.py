65ef68269b5bf52eafda9ba5da3ae0c1958e92c7
FedericoGregori <jfedericogregori@gmail.com>
2020-05-19 13:57:27 -0300

    [ADD] Manifest && Gitignore

---
+++ __manifest__.py
@@ -0,0 +1,42 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Calyx Odoo Module',
+    'summary': """
+        Summary of the module's purpose""",
+
+    'author': 'Calyx Servicios S.A., Odoo Community Association (OCA)',
+    'maintainers': ['<Github/Gitlab Username/s>'],
+
+    'website': 'http://www.calyxservicios.com.ar/',
+    'license': 'AGPL-3',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '11.0.1.0.0',
+    # see https://odoo-community.org/page/development-status
+    'development_status': 'Production/Stable',
+
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
